9a3b642fc68925649a17ce2c807e145f60d9fbfd
unknown <mock.jablonski@gmail.com>
2017-01-23 21:25:53 +0000

    Create a window.

--- src/App_Cat.java
+++ src/App_Cat.java
@@ -1,9 +1,25 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class App_Cat {
+
+public class App_Cat extends JFrame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new App_Cat();
 
+	}
+	
+	public App_Cat() {
+
+		this.setLocationRelativeTo(null);
+		this.setTitle("Cat_UI v0.1");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		JPanel thePanel = new JPanel();
+		
+		
+		this.add(thePanel);
+		this.setVisible(true);
 	}
 
 }
